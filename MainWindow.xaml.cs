871f9fc90216b6990eb6c7a087d2007b4e9d2fb4
David Montano <cuatro04sr@gmail.com>
2022-01-28 18:06:01 -0500

    Remove ignored files

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿/// Daniela Olarte Borja || David Montaño Tamayo
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,42 @@ namespace stats_s1
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() => InitializeComponent();
+
+        private void Import_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            var fileDialog = new Microsoft.Win32.OpenFileDialog() { Filter = "CSV Files (*.csv)|*.csv", Title = "Open File" };
+            var result = fileDialog.ShowDialog();
+            if (result == false) return;
+            lvUsers.ItemsSource = ReadCSV(fileDialog.FileName);
+        }
+
+        private IEnumerable<Users> ReadCSV(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            return lines.Select(line =>
+            {
+                string[] data = line.Split(',');
+                return new Users(Convert.ToInt32(data[0]), Convert.ToInt32(data[1]), data[2], data[3], data[4]);
+            });
+        }
+    }
+
+    public class Users
+    {
+        public int CodDpto { get; }
+        public int CodMcpio { get; }
+        public string NameDpto { get; }
+        public string NameMcpio { get; }
+        public string Type { get; }
+
+        public Users(int codDpto, int codMcpio, string nameDpto, string nameMcpio, string type)
+        {
+            CodDpto = codDpto;
+            CodMcpio = codMcpio;
+            NameDpto = nameDpto;
+            NameMcpio = nameMcpio;
+            Type = type;
         }
     }
 }
