7fae1b047b8b18705997a89f25c0c6285b09f228
David Montano <cuatro04sr@gmail.com>
2022-01-29 01:34:35 -0500

    feat: add filter by cod mcpio on list view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,23 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
 
 namespace stats_s1
 {
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+        ObservableCollection<Users> Users;
 
         private void Import_Click(object sender, RoutedEventArgs e)
         {
@@ -30,24 +25,43 @@ namespace stats_s1
                 MessageBox.Show("Failed to Load Data!", "Import Data", MessageBoxButton.OK, MessageBoxImage.Error);
             } else
             {
-                lvUsers.ItemsSource = ReadCSV(fileDialog.FileName);
+                ReadCSV(fileDialog.FileName);
+                TBCodInitial.IsEnabled = true;
+                TBCodFinal.IsEnabled = true;
                 MessageBox.Show("Data Loaded!", "Import Data", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
-        private IEnumerable<Users> ReadCSV(string fileName)
+        private void ReadCSV(string fileName)
         {
             string[] lines = File.ReadAllLines(fileName);
-            return lines.Select(line =>
+            IEnumerable<Users> IUsers = lines.Select(line =>
             {
                 string[] data = line.Split(',');
                 return new Users(Convert.ToInt32(data[0]), Convert.ToInt32(data[1]), data[2], data[3], data[4]);
             });
+            Users = new ObservableCollection<Users>(IUsers);
+            lvUsers.ItemsSource = Users;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            ObservableCollection<Users> tempUsers = new ObservableCollection<Users>();
+            int start = (TBCodInitial.Text.Length > 0) ? Convert.ToInt32(TBCodInitial.Text) : 0;
+            int final = (TBCodFinal.Text.Length > 0) ? Convert.ToInt32(TBCodFinal.Text) : int.MaxValue;
+            foreach (Users users in Users)
+            {
+                if (users.CodMcpio >= start && users.CodMcpio <= final)
+                    tempUsers.Add(users);
+            }
+            lvUsers.ClearValue(ItemsControl.ItemsSourceProperty);
+            lvUsers.ItemsSource = tempUsers;
         }
     }
 
